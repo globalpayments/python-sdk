4a02acc30ef9fb4190a31444495b0c1134fca7bf
Tony Smedal <mark.smedal@heartland.us>
2023-04-20 08:29:30 -0400

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='GlobalPayments.Api',
-    version='1.0.5',
+    version='1.0.6',
     author='Heartland Payment Systems',
     author_email='EntApp_DevPortal@e-hps.com',
     packages=[
